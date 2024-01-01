71c6e8d668ae6e0c65d7fdb66c12470d6616ef26
Ismail <ismail.hamdi@zeply.com>
2024-01-01 23:57:50 +0100

    chore: :lock: enable cors

--- server/app/app.ts
+++ server/app/app.ts
@@ -1,13 +1,43 @@
 import fastify from "fastify";
+import cors from "@fastify/cors";
+
 import * as routes from "./routes";
 import * as plugins from "./plugins";
 
 const createApp = () => {
   const app = fastify();
 
+  app.addHook("onRequest", (req, _, done) => {
+    req.log.info(
+      {
+        method: req.raw.method,
+        url: req.raw.url,
+      },
+      "incoming http request"
+    );
+    done();
+  });
+
+  app.addHook("onResponse", (req, reply, done) => {
+    req.log.info(
+      {
+        responseTime: reply.getResponseTime(),
+        statusCode: reply.statusCode,
+        url: req.raw.url,
+      },
+      "http request completed"
+    );
+    done();
+  });
+
   //plugin
+  app.register(cors, {
+    origin: true,
+    credentials: true
+  });
+
   app.register(plugins.envalid);
-  app.register(plugins.octokit)
+  app.register(plugins.octokit);
 
   //routes
   app.register(routes.health, { prefix: "/" });
--- server/app/server.ts
+++ server/app/server.ts
@@ -4,6 +4,7 @@ import { env } from "./plugins/envalid/envalid.plugin";
 const app = createApp();
 const start = async () => {
   try {
+    console.log(`server listening on ${env.PORT}`)
     await app.listen({ host: "0.0.0.0", port: env.PORT });
   } catch (error) {
     app.log.error(error);
